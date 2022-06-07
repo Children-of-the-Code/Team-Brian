f2af50a6affc22333c59014afa349a274a343dcf
Brian Urban <brian524@revature.net>
2022-06-07 10:38:03 -0500

    Project Updated

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -2,8 +2,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Driver {
-    public static void main(String[] args){
-        SpringApplication.run(Driver.class);
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
     }
 }
